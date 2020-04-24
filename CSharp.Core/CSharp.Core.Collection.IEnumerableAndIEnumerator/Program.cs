12734aef22604cf2f75b1363b24d0b0e2a850357
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-04-24 16:33:51 +0800

    init repo

---
+++ CSharp.Core/CSharp.Core.Collection.IEnumerableAndIEnumerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharp.Core.Collection.IEnumerableAndIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string 实现了 IEnumerable
+            string s = "Hello World";
+            // 获取一个全新的枚举器。 string中的枚举器为 CharEnumerator，其实现了 IEnumerator
+            var enumerator = s.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
