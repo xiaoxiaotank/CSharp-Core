3ab2f22beaf6fb7ce30c25dc3144a227d75594cd
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-04-24 18:45:44 +0800

    新增ICollection demo

---
+++ CSharp.Core/CSharp.Core.Collection.ICollection/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharp.Core.Collection.ICollection1
+{
+    class Program
+    {
+        /// <summary>
+        /// 需要注意的是 
+        ///     ICollection<T> 并没有实现 ICollection
+        ///     两者之间的定义相差巨大
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+           
+        }
+
+        private static void ErrorImplementICollection()
+        {
+            var collection = new ErrorCollection<int>();
+            string[] objs = new string[collection.Count];
+            // 很显然，int 类型的集合复制给 string[]， 这破坏了静态类型安全性
+            collection.CopyTo(objs, 0);
+        }
+    }
+
+#warning 错误的Collection实现方式
+    class ErrorCollection<T> : ICollection<T>, ICollection
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharp.Core/CSharp.Core.Collection.IList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharp.Core.Collection.IList1
+{
+    class Program
+    {
+#warning 不能同时实现 IList<T> 和 IList 接口，会破坏静态类型安全性（参考ICollection）
+
+        /// <summary>
+        /// 需要注意的是 
+        ///     IList<T> 并没有实现 IList
+        ///     两者之间的定义相差巨大
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
