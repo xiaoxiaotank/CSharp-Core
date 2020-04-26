29e6ed5488e7ff938a6263831dcf48554dbe4718
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-04-26 17:15:17 +0800

    add IOrderedEnumerable

---
+++ CSharp.Core/CSharp.Core.Collection.IOrderedEnumerable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Core.Collection.IOrderedEnumerable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOrderedEnumerable<int> orderedEnumerable;            
+        }
+    }
+}
