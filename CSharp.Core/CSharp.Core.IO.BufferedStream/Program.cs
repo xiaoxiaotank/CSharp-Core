d466a76239b93576ee59e726d67821d293c52f7a
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2020-07-24 15:08:35 +0800

    新增BufferedStream

---
+++ CSharp.Core/CSharp.Core.IO.BufferedStream/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace CSharp.Core.IO._BufferedStream
+{
+    class Program
+    {
+        // BufferedStream：用于装饰或包装另一个具有缓冲功能的流
+        static void Main(string[] args)
+        {
+            using var fs = File.OpenRead("test.txt");
+            // 1KB
+            using var bs = new BufferedStream(fs, 1024);
+            bs.ReadByte();
+            // 可以看到 fs 的位置已经前进了 1024（如果长度足够），这就表明fs已经将部分流缓存到了bs中
+            Console.WriteLine(fs.Position);
+        }
+    }
+}
